abcf5e80b5bc919550570482bce72f1806b7546b
Rohit Farmer <bioinformaticsbot@gmail.com>
2018-03-24 16:48:06 -0500

    count.pl

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# bioinformatics-toolbox
+# Bioinformatics Toolbox
+
+Collection of scripts written in Perl or Python for some everyday as well as specific Bioinformatics Tasks.
+
+count.pl : Counts the number of sequences in a multiple FASTA file.
+Usage : count.pl <name.fasta>
---
+++ count.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl 
+
+=Synopsis
+DESCRIPTION		: Counts the number of sequences in a multiple FASTA file.
+DEPENDENCIES	: none
+USAGE           : count.pl <name.fasta>
+INPUT			: name.fasta
+OUTPUT			: Number of FASTA sequences: <number>
+AUTHOR			: Dr. Rohit Farmer
+LAST MODIFIED	: 24/05/2016
+=cut
+
+use strict;
+use warnings;
+
+# Usage check
+if (   $#ARGV < 0
+	or $ARGV[0] eq 'help'
+	or $ARGV[0] eq 'h' )
+{
+	print "Usage: count.pl input.fasta\n";
+	exit -1;
+}
+
+# Open file
+open( FILE, "$ARGV[0]" ) || die "Can't open file";
+
+# Count the number of FASTA sequences
+my $count;
+while (<FILE>) {
+	if ( $_ =~ m/^>/ ) {
+		$count++;
+	}
+}
+
+# Output
+print "Number of FASTA sequences in $ARGV[0]: $count\n";
+close(FILE);
